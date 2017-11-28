3ccc327e062100b60666a88bac53f829b22e3ce2
Rafael dos Santos Calderani <rafael.calderani@gmail.com>
2017-11-27 22:44:39 -0200

    aula 01

---
+++ TopicoAssets/Assets/Scripts/Bloco.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bloco : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (Vector3.back);
+	}
+
+	void OnCollisionEnter2D(Collision2D c) {
+		print ("Eita mano o " + c.collider.tag + " bateu no " + c.otherCollider);
+
+		if (c.gameObject.tag == "Player") {
+			Destroy (gameObject);
+		}
+	}
+
+	void OnCollisionStaty2D(Collision2D c) {
+		print ("Ainda tá relando jão...");
+	}
+
+	void OnCollisionExit2D(Collision2D c) {
+		print ("Parou.");
+	}
+}
---
+++ TopicoAssets/Assets/Scripts/Personagem.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Personagem : MonoBehaviour {
+	public float velocidade;
+
+	// Use this for initialization
+	void Start () {
+		// executa uma única vez
+		//print("começou out baguio mano");
+		//transform.position = new Vector2 (-10.0f, 0.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// fica em loop
+		//transform.Translate (3.0f * Time.deltaTime, 0.0f, 0.0f);
+		//transform.Rotate (Vector3.forward*velocidade*Time.deltaTime);
+		//print (Input.GetAxis("Horizontal"));
+		float moveX = Input.GetAxis("Horizontal") * velocidade * Time.deltaTime;
+		float moveY = Input.GetAxis("Vertical") * velocidade * Time.deltaTime;
+		transform.Translate (moveX, moveY, 0.0f);
+
+		// Input com retorno booleano
+		if (Input.GetButtonDown("Jump")) {
+			transform.position = Vector2.zero;
+
+			SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+			spriteRenderer.flipX = !spriteRenderer.flipX;
+		}
+	}
+}
