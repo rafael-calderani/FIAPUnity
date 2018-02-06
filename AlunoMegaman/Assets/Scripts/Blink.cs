77cddbd58183732d3c4ff7813d2a56189a55f70f
logonrm <logonrm@fiap.com.br>
2018-02-05 22:47:46 -0200

    exemplo de animação e game plataforma parte 01

---
+++ AlunoMegaman/Assets/Scripts/Blink.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Blink : MonoBehaviour {
+	public float intervalo;
+
+	// Use this for initialization
+	IEnumerator Start () {
+		SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer> ();
+
+		spriteRenderer.enabled = !spriteRenderer.enabled;
+		yield return new WaitForSeconds(intervalo);
+
+		StartCoroutine (Start ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Return)) { // Enter pressionado
+			SceneManager.LoadScene("GameScene");
+		}
+
+	}
+}
---
+++ AlunosAnimacao/Assets/Scripts/AnimaScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimaScript : MonoBehaviour {
+
+	public float velocidade;
+	Animator animator;
+	SpriteRenderer spriteRenderer;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator> ();
+		spriteRenderer = GetComponent<SpriteRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// pega o input de movimento horizontal do jogador
+		float input = Input.GetAxisRaw ("Horizontal");
+		float mover_x = input * velocidade * Time.deltaTime;
+		transform.Translate (mover_x, 0.0f, 0.0f); // move o personagem
+
+		// Orientação do personagem
+		if (mover_x != 0) {
+			spriteRenderer.flipX = (mover_x < 0.0f);
+		}
+
+		// Reproduz animação de movimento do personagem
+		animator.SetFloat("pMover", Mathf.Abs(input));
+	}
+}
