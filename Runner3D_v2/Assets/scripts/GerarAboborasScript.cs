d2b5c8d3417cb46c3673a00143a7170bbd018f5c
logonrm <logonrm@fiap.com.br>
2018-03-12 22:51:33 -0300

    Zombie pumpkin catcher - initial commit

---
+++ Runner3D_v2/Assets/scripts/GerarAboborasScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GerarAboborasScript : MonoBehaviour {
+	public float limiteEsquerdo, limiteDireito, limiteFrontal, limiteTraseiro;
+	public float frequencia;
+	public GameObject aboboraPrefab;
+
+	// Use this for initialization
+	IEnumerator Start () {		
+		yield return new WaitForSeconds (Random.Range(0.5f, frequencia));
+		Vector3 pos = new Vector3 ();
+		pos.x = Random.Range (limiteEsquerdo, limiteDireito);
+		pos.y = transform.position.y;
+		pos.z = Random.Range (limiteFrontal, limiteTraseiro);
+		Instantiate (aboboraPrefab, pos, transform.rotation);
+
+		StartCoroutine (Start ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Runner3D_v2/Assets/scripts/PersonagemScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonagemScript : MonoBehaviour {
+	
+	Vector3 novaPosicao;
+	Animation animacao;
+
+	public float velocidade;
+	public GameObject personagem;
+
+	// Use this for initialization
+	void Start () {
+		// captura a posição inicial do personagem
+		novaPosicao = transform.position;
+
+		// define a animacao inicial do personagem
+		animacao = personagem.GetComponent<Animation>();
+		animacao.CrossFade ("idle");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Touch ou click do mouse
+		if (Input.GetButton("Fire1")) {
+
+			// Captura as coordenadas do clique ou touch
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+
+			if (Physics.Raycast (ray, out hit) && (hit.collider.gameObject.tag != "Player")) {
+				
+				novaPosicao = new Vector3(hit.point.x, novaPosicao.y, hit.point.z);
+
+				// move o personagem
+				transform.position = Vector3.MoveTowards (
+					transform.position,
+					novaPosicao, velocidade * Time.deltaTime);
+				
+				transform.LookAt (hit.point);
+
+				animacao.CrossFade ("run");
+
+			} else {
+
+				animacao.CrossFade ("idle");
+			}
+		}
+	}
+
+	void OnCollisionEnter(Collision c){
+		if (c.gameObject.tag == "Item") {
+			Destroy (c.gameObject);
+		}
+	}
+}
